2a037c5a14a7a0d2f436d20f92a0fa1dd4d65688
paolobettelini <paolo.bettelini@samtrevano.ch>
2022-09-10 21:55:07 +0200

    version 1.1

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Anti-Youtube Shortss
 
-Youtube Shorts suck. No timeline? No volume? Cannot decide the next video?
-Unless it's the only way to survive a Jigsaw death trap I'm not watching TikTok
+Youtube Shorts suck. No timeline. No volume bar. Cannot decide the next video.
+
+## Installation
+
+```bash
+git clone https://github.com/paolobettelini/anti-youtube-shorts
+cd anti-youtube-shorts
+web-ext build
+```
+Disable addons verification (needs Firefox Nightlxy and Developer channel).
+<br>
+Set `xpinstall.signatures.required` to `False` in `about:config`
+<br>
+Go to `about:addons` and drop the `.zip` file in the tab.
--- redirect.js
+++ redirect.js
@@ -1,5 +1,34 @@
 const value = 'shorts/';
 const replacement = 'watch?v='
 
-let url = window.location.href.replace(value, replacement);
-window.location.href = url;
+function redirectShort() {
+    let url = window.location.href;
+    if (url.includes(value)) {
+        window.location.href = url.replace(value, replacement);
+    }
+}
+
+redirectShort(); // before page load
+
+// from https://stackoverflow.com/questions/3522090/event-when-window-location-href-changes
+var oldHref = document.location.href;
+window.onload = function() {
+    var bodyList = document.querySelector("body")
+
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (oldHref != document.location.href) {
+                oldHref = document.location.href;
+
+                redirectShort();
+            }
+        });
+    });
+    
+    var config = {
+        childList: true,
+        subtree: true
+    };
+    
+    observer.observe(bodyList, config);
+};
